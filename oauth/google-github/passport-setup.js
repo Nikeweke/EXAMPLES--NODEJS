b4622d2cab0ccd28a7c340aa58db9fd1f2a0710c
Nikeweke <Driver3@meta.ua>
2019-05-10 16:54:54 +0300

    Update passport-setup.js

--- oauth/google-github/passport-setup.js
+++ oauth/google-github/passport-setup.js
@@ -3,44 +3,38 @@ const passport       = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
 const GitHubStrategy = require('passport-github')
 
-// =============================================> Google 
-const googleOptions = {
-  // callbackURL - этот же путь, но абсолютный(http://localhost:8000/google_redirect) 
-  // должен быть в Google console "Разрешенные URI перенаправления".
-  // Это может быть и главная страница
-  callbackURL : '/google_redirect', 
-  clientID    : keys.google.clientID,
-  clientSecret: keys.google.clientSecret
-}
-const googleCallback = (accessToken, refreshToken, profile, done) => {
-  console.log('ACCESS TOKEN =', accessToken)
-  console.log('REFRESH TOKEN =', refreshToken)
-  console.log('PROFILE = ', profile)
-
-  /*
-  #  Оперировать(добавлять/искать в БД) информацией пользователя нужно здесь, 
-  #  дальше инфа не идет(то есть в обработчик редиректа)  
-  */
-
-  // без вызова этой функции не идет дальше выполнение программы
-  done(null, profile) 
-}
-passport.use(new GoogleStrategy(googleOptions, googleCallback))
+// Google 
+const googleStrategy = new GoogleStrategy(
+  {
+    // callbackURL - этот же путь, но абсолютный(http://localhost:8000/google_redirect) 
+    // должен быть в Google console "Разрешенные URI перенаправления".
+    // Это может быть и главная страница
+    callbackURL : '/google_redirect', 
+    clientID    : keys.google.clientID,
+    clientSecret: keys.google.clientSecret
+  }, 
+  function (accessToken, refreshToken, profile, done) {
+    // без вызова этой функции не идет дальше выполнение программы
+    return done(null, profile) 
+  }
+)
 
 
-// =============================================> Github
-const githubOptions = {
-  callbackURL: '/github_redirect',
-  clientID    : keys.github.clientID,
-  clientSecret: keys.github.clientSecret
-}
-const githubCallback = (accessToken, refreshToken, profile, done) => {
-  console.log('ACCESS TOKEN =', accessToken)
-  console.log('REFRESH TOKEN =', refreshToken)
-  console.log('PROFILE = ', profile)
-  done(null, profile) 
-} 
-passport.use(new GitHubStrategy(githubOptions, githubCallback))
+// Github
+const gitHubStrategy = new GitHubStrategy(
+  {
+    callbackURL: '/github_redirect',
+    clientID    : keys.github.clientID,
+    clientSecret: keys.github.clientSecret
+  },
+  function (accessToken, refreshToken, profile, done) {
+    return done(null, profile) 
+  }
+)
+
+
+passport.use(googleStrategy)
+passport.use(gitHubStrategy)
 
 
 // без реализации этой функции - выдает ошибку
