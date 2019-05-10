1bdc5a7bd9084b587f7b43cf7e4d833394a5d4e9
Nikeweke <Driver3@meta.ua>
2019-05-10 16:55:25 +0300

    Update app.js

--- oauth/google-github/app.js
+++ oauth/google-github/app.js
@@ -1,6 +1,23 @@
 const express  = require('express')
+const session  = require('express-session') // server keeps session info, client keeps only token
 const passport = require('./passport-setup')
 const app      = express()
+
+const sess = {
+  secret: 'SecretSuper',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+}
+
+app.use(session(sess))
+app.use(passport.initialize())
+
+function secure (req, res, next) {
+	if (req.session.user) { return next() }
+  res.status(401).json({message: 'You are not authorized'});
+}
+
 
 // home
 app.get('/', (req, res) => {
@@ -10,40 +27,66 @@ app.get('/', (req, res) => {
 		<a href="/github_login">Github login</a>
 	`)
 })
-app.get('/fail', (req, res) => {
-  res.send("Something went wrong")
+
+// user info (secured)
+app.get('/user', [secure], (req, res) => {
+	const {user}     = req.session 
+	const {provider} = req.query
+	let displayName
+	
+	if (provider === 'google') {
+		displayName = user.displayName
+	} else {
+		displayName = user.username
+	}
+
+	res.send(`
+		<h2>You are authed via ${provider}</h2>
+		<h4>Your name is = ${displayName}</h4>
+		${JSON.stringify(user, null, 2)} <br><br>
+		<a href="/logout">Logout</a>
+  `)
+})
+
+app.get('/logout', (req, res) => {
+	req.session.user = '' 
+	res.redirect('/')
 })
 
 
-// AUTH routes
-app.use(passport.initialize())
-
+// ==========================> AUTH routes
 // google-auth
 app.get('/google_login', passport.authenticate('google', {
 	scope: ['profile'] // запрашиваемые данные от гугла от пользователя
 }))
-app.get('/google_redirect', passport.authenticate('google', { failureRedirect: '/fail' }), (req, res) => {
-	res.send(`
-		<h3>Authed by google successfuly</h3>
-		<a href="/">Get back</a>
-	`)
-})
 
 // github-auth
 app.get('/github_login', passport.authenticate('github', {
 	scope: ['profile'] 
 }))
-app.get('/github_redirect', passport.authenticate('github', { failureRedirect: '/fail' }), (req, res) => {
-	res.send(`
-		<h3>Authed by github successfuly</h3>
-		<a href="/">Get back</a>
-	`)
-})
 
+// custom callback for redirects
+const customRedirectHandler = function (strategy) {
+  return (req, res, next) => {
+		passport.authenticate(strategy, (err, user, info) => {
+			if (err) { return next(err) }
+			if (!user) { return res.redirect('/') }
+			
+			req.logIn(user, (err) => {
+				if (err) { return next(err) }
+				req.session.user = user
+				res.redirect(`/user?provider=${strategy}`)
+			})
+		})(req, res, next)
+	}
+}
+
+app.get('/google_redirect', customRedirectHandler('google'))
+app.get('/github_redirect', customRedirectHandler('github'))
 
 
 // Launch our app
 const port = 8000;
 const server = app.listen(port, function () {
-    console.log('JWT listening on port ' + port);
+    console.log('Server listening on port ' + port);
 });
