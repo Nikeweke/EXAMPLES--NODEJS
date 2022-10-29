35bc19d2a47aa23f4ad36cda25d69943e2c10cf5
Nikeweke <jxammet@gmail.com>
2022-10-29 21:49:23 +0300

    Update jwt-auth.guard.ts

--- nestjs-with-passports-and-arch/src/components/auth/guards/jwt-auth.guard.ts
+++ nestjs-with-passports-and-arch/src/components/auth/guards/jwt-auth.guard.ts
@@ -1,43 +1,5 @@
-import { NotAuthorizedCodeEnum } from '../enums/not-authorized-codes.enum';
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private readonly reflector: Reflector) {
-    super();
-  }
-
-  canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.get<boolean>(
-      'isPublic',
-      context.getHandler(),
-    );
-
-    if (isPublic) {
-      return true;
-    }
-
-    return super.canActivate(context);
-  }
-
-  handleRequest(err, user, info) {
-    if (err) {
-      throw err;
-    }
-
-    if (info) {
-      if (info.name === 'TokenExpiredError') {
-        // eslint-disable-next-line no-param-reassign
-        info.code = NotAuthorizedCodeEnum.accessTokenExpired;
-        // eslint-disable-next-line no-param-reassign
-        info.status = 401;
-      }
-
-      throw new UnauthorizedException(info);
-    }
-
-    return user;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
